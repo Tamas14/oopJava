b420b2611b03fb212e47750aaef2787b6e789f4c
Tamás Csaba <tamas14@iit.uni-miskolc.hu>
2018-03-02 18:25:35 +0100

    Homework

---
+++ Lessons/2018. 03. 01/Calculator v2/src/hu/unimiskolc/Main.java
@@ -0,0 +1,122 @@
+package hu.unimiskolc;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		char[] operators = { '+', '-', '*', '/' };
+		String response;
+		
+		do {
+			methodTwo(s, operators);
+			System.out.print("Want to calculate another one? (yes/no): ");
+			response = s.nextLine();
+		} while (parseRequest(response));
+
+		s.close();
+	}
+
+	public static void methodOne(Scanner s, char[] operators) {
+		System.out.print("Number one: ");
+		double n1 = s.nextDouble();
+
+		System.out.print("Operation: ");
+		String op = s.next();
+
+		System.out.print("Number two: ");
+		double n2 = s.nextDouble();
+
+		if (op.equals("/") && n2 == 0) {
+			System.out.println("Cannot divide with 0");
+			return;
+		}
+
+		double value = 0;
+		boolean done = false;
+
+		for (char c : operators) {
+			if ((c + "").equals(op)) {
+				value = calculate(n1, op, n2);
+				done = true;
+			}
+		}
+
+		if (!done) {
+			System.out.println("Bad operator");
+			return;
+		}
+
+		System.out.println(n1 + " " + op + " " + n2 + " = " + value);
+	}
+
+	public static void methodTwo(Scanner s, char[] operators) {
+		System.out.print("Input (a + b): ");
+		String input = s.nextLine();
+
+		double n1, n2;
+		String op;
+
+		try {
+			n1 = Double.parseDouble(input.split(" ")[0]);
+			op = input.split(" ")[1];
+			n2 = Double.parseDouble(input.split(" ")[2]);
+		} catch (NumberFormatException e) {
+			System.out.println("Use the format shown before.");
+			return;
+		}
+
+		if (op.equals("/") && n2 == 0) {
+			System.out.println("Cannot divide with 0");
+			return;
+		}
+
+		double value = 0;
+		boolean done = false;
+
+		for (char c : operators) {
+			if ((c + "").equals(op)) {
+				value = calculate(n1, op, n2);
+				done = true;
+			}
+		}
+
+		if (!done) {
+			System.out.println("Bad operator");
+			return;
+		}
+
+		System.out.println(input + " = " + value);
+	}
+
+	public static double calculate(double n1, String op, double n2) {
+		switch (op) {
+			case "+": {
+				return n1 + n2;
+			}
+			case "-": {
+				return n1 - n2;
+			}
+			case "*": {
+				return n1 * n2;
+			}
+			case "/": {
+				return n1 / n2;
+			}
+		}
+
+		return 0;
+	}
+
+	public static boolean parseRequest(String s) {
+		String[] repeat = {"yes", "y", "igen", "i"};
+
+		for (String str : repeat)
+			if (str.equalsIgnoreCase(s))
+				return true;
+		
+		return false;
+	}
+
+}
