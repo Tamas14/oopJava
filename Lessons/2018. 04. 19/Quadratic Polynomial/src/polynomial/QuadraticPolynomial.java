9cb9f4a871cd0447b504df9dd81ca58ee9b58524
Tamás Csaba <tamas14@iit.uni-miskolc.hu>
2018-04-19 23:36:55 +0200

    Upload homework

--- Lessons/2018. 04. 19/Quadratic Polynomial/src/polynomial/QuadraticPolynomial.java
+++ Lessons/2018. 04. 19/Quadratic Polynomial/src/polynomial/QuadraticPolynomial.java
@@ -1,15 +1,63 @@
 package polynomial;
 
+import java.text.DecimalFormat;
+
 import math.Polynomial;
 
-public class QuadraticPolynomial implements Polynomial {
+public class QuadraticPolynomial implements Polynomial
+{
 	double a, b, c;
 
-	public enum Root {
+	public final class Complex
+	{
+		private final double re;
+		private final double im;
+
+		public Complex(double real, double imag)
+		{
+			re = real;
+			im = imag;
+		}
+
+		public String toString()
+		{
+			double re = format(this.re);
+			double im = format(this.im);
+
+			if (im == 0)
+				return re + "";
+			if (re == 0)
+				return im + "i";
+			if (im < 0)
+				return re + " - " + -im + "i";
+			return re + " + " + im + "i";
+		}
+
+		public double format(double num)
+		{
+			DecimalFormat df = new DecimalFormat("#.####");
+			String tmp = df.format(num).replace(',', '.');
+			return Double.parseDouble(tmp);
+		}
+
+		public double re()
+		{
+			return re;
+		}
+
+		public double im()
+		{
+			return im;
+		}
+	}
+
+	public enum Root
+	{
 		COMPLEX, REAL_ONE, REAL_TWO;
 	}
 
-	public QuadraticPolynomial(double a, double b, double c) {
+	public QuadraticPolynomial(double a, double b, double c)
+	{
 		if (a == 0)
 			return;
 
@@ -19,26 +67,31 @@ public class QuadraticPolynomial implements Polynomial {
 	}
 
 	@Override
-	public double getY(double x) {
+	public double getY(double x)
+	{
 		return a * x * x + b * x + c;
 	}
 
 	@Override
-	public int getDegree() {
+	public int getDegree()
+	{
 		return 2;
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return "y = " + a + "x^2 " + ((b >= 0) ? "+ " : "- ") + Math.abs(b) + "x " + ((c >= 0) ? "+ " : "- ")
 				+ Math.abs(c);
 	}
 
-	public double getDiscriminant() {
-		return b * b - 4 * a * c;
+	public double getDiscriminant()
+	{
+		return (b * b) - (4 * a * c);
 	}
 
-	public Root getRootType() {
+	public Root getRootType()
+	{
 		double d = getDiscriminant();
 
 		if (d < 0)
@@ -47,6 +100,52 @@ public class QuadraticPolynomial implements Polynomial {
 			return Root.REAL_TWO;
 		else
 			return Root.REAL_ONE;
+	}
+
+	public Complex[] getRoots()
+	{
+		Root type = getRootType();
+		double dis = getDiscriminant();
+
+		Complex[] roots = new Complex[2];
+
+		switch (type)
+		{
+			case COMPLEX:
+			{
+				double realPart = -b / (2 * a);
+				double imaginaryPart = Math.sqrt(-dis) / (2 * a);
+
+				roots[0] = new Complex(realPart, imaginaryPart);
+				roots[1] = new Complex(realPart, -imaginaryPart);
+
+				break;
+			}
+			case REAL_ONE:
+			{
+				double root = (-b + Math.sqrt(dis)) / (2 * a);
+
+				roots[0] = new Complex(root, 0);
+				// roots[1] = new Complex(root, 0);
+
+				break;
+			}
+			case REAL_TWO:
+			{
+				double root = (-b + Math.sqrt(dis)) / (2 * a);
+				double root2 = (-b - Math.sqrt(dis)) / (2 * a);
+
+				roots[0] = new Complex(root, 0);
+				roots[1] = new Complex(root2, 0);
+
+				break;
+			}
+			default:// Cannot get here
+				break;
+
+		}
+
+		return roots;
 	}
 
 }
--- Lessons/2018. 04. 19/Quadratic Polynomial/src/polynomial/Test.java
+++ Lessons/2018. 04. 19/Quadratic Polynomial/src/polynomial/Test.java
@@ -1,9 +1,41 @@
 package polynomial;
 
-public class Test {
-	public static void main(String[] args) {
-		QuadraticPolynomial qp = new QuadraticPolynomial(1, -3, 0);
-		
-		System.out.print(qp.toString());
+import java.util.Scanner;
+
+import polynomial.QuadraticPolynomial.Complex;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+
+		Scanner s = new Scanner(System.in);
+		QuadraticPolynomial[] polyArr = new QuadraticPolynomial[5];
+
+		System.out.println("Quadratic Polynomials:");
+		for (int i = 0; i < polyArr.length; i++)
+		{
+			System.out.print((i + 1) + ") A: ");
+			double a = Double.parseDouble(s.nextLine());
+			System.out.print((i + 1) + ") B: ");
+			double b = Double.parseDouble(s.nextLine());
+			System.out.print((i + 1) + ") C: ");
+			double c = Double.parseDouble(s.nextLine());
+			polyArr[i] = new QuadraticPolynomial(a, b, c);
+		}
+
+		s.close();
+
+		for (QuadraticPolynomial poly : polyArr)
+		{
+			System.out.println("Solving: " + poly.toString());
+
+			for (Complex root : poly.getRoots())
+			{
+				if (root != null)
+					System.out.println(root.toString());
+			}
+		}
+
 	}
 }
